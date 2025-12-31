67157b321862427831bdf8ce47cd1ec8ae0fd271
souravpattnaik431 <sauravpattnaik674@gmail.com>
2025-12-31 22:26:17 +0530

    first commit

--- src/main/java/org/ai/gemini/geminiai/ServletInitializer.java
+++ src/main/java/org/ai/gemini/geminiai/ServletInitializer.java
@@ -1,12 +1,13 @@
 package org.ai.gemini.geminiai;
 
+import org.jspecify.annotations.NonNull;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 public class ServletInitializer extends SpringBootServletInitializer {
 
     @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+    protected @NonNull SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(GeminiAiApplication.class);
     }
 
